33be03a78c9ff8a023a18679f473875943c49b41
Bryan Bizarro <bryan.bizarro@ug.uchile.cl>
2016-12-07 11:22:45 -0300

    a
    aa

--- monitor_de_motores/monitor_de_motores.ino
+++ monitor_de_motores/monitor_de_motores.ino
@@ -174,6 +174,7 @@ void loop() {
   if(canId == sendIdKelly1){
     if(engineData/10 == 2){         // Si el primer digito de engineData es '1' se proceden a leer corrientes y voltajes.
 <<<<<<< HEAD
+<<<<<<< HEAD
       dataToSend[2] = 1;
       dataToSend[3] = 01;
       for(int j = 5; j < 12; j++){
@@ -222,6 +223,20 @@ void loop() {
       engineData = 5*10 + engineData%10;
     } else if(engineData/10 == 6){                        // Si el 1er digito de engineData es '3' se procede a leer la temperatura.
 >>>>>>> parent of 13434e0... actualizacion motores
+=======
+      Serial.print("I1_A,");Serial.print(buff[0]);Serial.print("\n");
+      Serial.print("I1_B,");Serial.print(buff[1]);Serial.print("\n");
+      Serial.print("I1_C,");Serial.print(buff[2]);Serial.print("\n");
+      Serial.print("V1_A,");Serial.print(buff[3]);Serial.print("\n");
+      Serial.print("V1_B,");Serial.print(buff[4]);Serial.print("\n");
+      Serial.print("V1_C,");Serial.print(buff[5]);Serial.print("\n");
+      engineData = 3*10 + engineData%10;
+    } else if(engineData/10 == 4){  // Si el 1er digito de engineData es '2' se procede a leer RPM.
+      Serial.print("ENG_RPM_1,");Serial.print((buff[0])<<8|buff[1]);Serial.print("\n");
+      Serial.print("ENG1_ERR_CODE,");Serial.print((buff[3])<<8|buff[4]);Serial.print("\n");
+      engineData = 5*10 + engineData%10;
+    } else if(engineData/10 == 6){                        // Si el 1er digito de engineData es '3' se procede a leer la temperatura.
+>>>>>>> parent of 13434e0... actualizacion motores
       Serial.print("ENG1_PWM,");Serial.print(buff[0]);Serial.print("\n");
       Serial.print("ENG1_EMR,");Serial.print(buff[1]);Serial.print("\n");
       Serial.print("ENG1_TEMP,");Serial.print(buff[2]);Serial.print("\n");      // Temperatura motor: Celcius
@@ -230,6 +245,7 @@ void loop() {
     } 
   } else if(canId == sendIdKelly2){
     if(engineData%10 == 2){         // 2do digito de engineData es 1, lee corrientes y voltajes
+<<<<<<< HEAD
 <<<<<<< HEAD
 
       dataToSend[2] = 1;
@@ -284,6 +300,20 @@ void loop() {
     } else if(engineData%10 == 0){     
                          
       Serial.print("ENG2_Current_Reverse_Switch_Status,");Serial.print(buff[0]);Serial.print("\n");      // Reverse Status
+=======
+      Serial.print("I2_A,");Serial.print(buff[0]);Serial.print("\n");
+      Serial.print("I2_B,");Serial.print(buff[1]);Serial.print("\n");
+      Serial.print("I2_C,");Serial.print(buff[2]);Serial.print("\n");
+      Serial.print("V2_A,");Serial.print(buff[3]);Serial.print("\n");
+      Serial.print("V2_B,");Serial.print(buff[4]);Serial.print("\n");
+      Serial.print("V2_C,");Serial.print(buff[5]);Serial.print("\n");
+      engineData = engineData/10*10 + 3;
+    } else if(engineData%10 == 4){  // 2do digito de engineData es 2, lee RPM.
+      Serial.print("ENG_RPM_2,");Serial.print((buff[0])<<8|buff[1]);Serial.print("\n");
+      engineData = engineData/10*10 + 5;
+    } else if(engineData%10 == 6){                        // 2do digito de engineData es 3, lee temperatura.
+      Serial.print("ENG_TEMP_2,");Serial.print(buff[2]);Serial.print("\n");      // Temperatura motor: Celcius
+>>>>>>> parent of 13434e0... actualizacion motores
 =======
       Serial.print("I2_A,");Serial.print(buff[0]);Serial.print("\n");
       Serial.print("I2_B,");Serial.print(buff[1]);Serial.print("\n");
